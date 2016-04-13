41c73f6ef73e265eddb6872359a8ce7534f72198
Jitendra Pandey <jpandey@buffalo.edu>
2016-04-13 03:24:37 -0400

    Added Debug statements

--- AOP_ProgrammaticApproach_AfterReturningAdvice/src/com/me/services/LogAfterService.java
+++ AOP_ProgrammaticApproach_AfterReturningAdvice/src/com/me/services/LogAfterService.java
@@ -10,15 +10,9 @@ import org.springframework.aop.MethodBeforeAdvice;
 import com.me.business.Bank;
 
 public class LogAfterService implements AfterReturningAdvice {
-
-
 	@Override
-	public void afterReturning(Object o1, Method m, Object[] param, Object o2) throws Throwable {
-		// TODO Auto-generated method stub
-//		System.out.println("=--"+m.getName());
-//		System.out.println(o1.toString());
-//		System.out.println(o2.toString());
-//		System.out.println(param);
+	public void afterReturning(Object ret, Method m, Object[] param, Object classObject) throws Throwable {
+
 		Log l = LogFactory.getLog(Bank.class);
 		l.info("Executing Service After Deposite Method");
 	}
